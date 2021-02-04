05431fb4b38ee7f071e8a9589c72b2acbc6e08cc
Jeffrey Ha <jha@ucsd.edu>
2021-02-04 03:33:50 -0800

    ya yeet

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+
+	$(".friend>h3>a").click(function (e) {
+		e.preventDefault();
+		var name = $(this).text();
+		$(this).text(anagrammedName(name));
+	});
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) {   
+	var name = request.query.name;
+	var description = request.query.description;
+
+	var newItem = {
+		name,
+		description,
+		imageURL: "https://picsum.photos/400/400"
+	};
+
+	console.log(newItem);
+	data.friends.push(newItem);
+	response.render('index', data);
+};
